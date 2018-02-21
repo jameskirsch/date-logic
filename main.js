e47f772c794cafbc7eab5078b801bee9920e1d04
jameskirsch <jjamesdk@yahoo.com>
2018-02-20 21:02:43 -0500

    displays discount price based on user selection

--- main.js
+++ main.js
@@ -1,20 +1,30 @@
+'use strict'; // this will display bad syntax as real errors, used for debugging
+
 // Sets the initial price, discount price, and days in advance into the DOM
 var price = 50.00;
 var discountPrice = 10.00;
-var daysInAdvance = 5;
+var daysInAdvance = 6;
 document.getElementById('price').innerHTML = price;
 document.getElementById('discountPrice').innerHTML = discountPrice;
 document.getElementById('daysInAdvance').innerHTML = daysInAdvance;
 
-function calculateTicketPrice() {
+// Need a way to add days to a given date, this is how to do it.
+function addDaysToDate(date, days) {
+    var result = new Date(date);
+    result.setDate(result.getDate() + days);
+    return result;
+}
 
-    // Get today's Date
+function calculateTicketPrice() {
+    console.clear();
+
+    // Get Today's Date
     var dateToday = new Date();
     var dd = dateToday.getDate();
     var mm = dateToday.getMonth() + 1; // January starts at 0
     var yyyy = dateToday.getFullYear();
 
-    // Formats months to always have a zero in the formatting if less than 10, 
+    // Format month to always have a zero in the formatting if less than 10
     // this can be done easier with jQuery
     if (mm < 10) {
         mm = '0' + mm;
@@ -22,17 +32,25 @@ function calculateTicketPrice() {
 
     // Format the Date for Today
     var today = new Date(yyyy + '-' + mm + '-' + dd);
+    console.log('Todays Date: ' + today);
 
     // Get The User's selection and the Projected Discount Date (days in advance from current date)
     var userSelectedTravelDate = new Date(document.getElementById('travelDate').value);
+    console.log('User Selected Travel Date: ' + userSelectedTravelDate);
+ 
+    var projectedDiscountDate = addDaysToDate(today, daysInAdvance);
+    console.log('Projected Discount Date: ' + projectedDiscountDate);
 
-    // The main logic
-    if (userSelectedTravelDate.getDate() > (today.getDate() + daysInAdvance)) {
-        console.log('discount');
+    var qaulifyingEventElement = document.getElementById('qualifyingEvent');
+    if (userSelectedTravelDate.getTime() > projectedDiscountDate.getTime()) {
+        qaulifyingEventElement.innerHTML = 'You Qualify for a Discount.';
+        qaulifyingEventElement.style.background = 'green';
+        document.getElementById('price').innerHTML = (price - discountPrice);
+        document.getElementById('price').style.background = 'lightgreen';
+    } else {
+        qaulifyingEventElement.innerHTML = 'You do not Qualify for a Discount.';
+        qaulifyingEventElement.style.background = 'grey';
+        document.getElementById('price').innerHTML = price;
+        document.getElementById('price').style.background = 'white';
     }
-
-    console.log(today.getDate() + daysInAdvance);
-
-    console.log('Todays Date: ' + today);
-    console.log('Selected Date: ' + userSelectedTravelDate);
 }
